e4b488c224041550f9d0557d74502a0c792b34f3
Anurag Uday Khanapurkar <x24214787@student.ncirl.ie>
2026-02-17 15:28:41 +0000

    Grass theme added

--- themes-widget/themes-widget.js
+++ themes-widget/themes-widget.js
@@ -97,6 +97,7 @@
                 <button data-category="night">Night</button>
                 <button data-category="mountain">Mountain</button>
                 <button data-category="lava">Lava</button>
+                <button data-category="Grass">Grass</button>
             </div>
         `;
         document.body.appendChild(panel);
